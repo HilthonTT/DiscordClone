6ea8320e1f4aea3e36f27bdb79aedeac1328d159
Timothy Tandt <Timothy.tandt@outlook.be>
2023-10-18 17:47:01 +0200

    [Feature] Added delete message
    Added ability to delete the message

--- discord-clone/components/chat/chat-item.tsx
+++ discord-clone/components/chat/chat-item.tsx
@@ -7,7 +7,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import Image from "next/image";
-import { KeyboardEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { ActionTooltip } from "@/components/action-tooltip";
 import { UserAvatar } from "@/components/user-avatar";
@@ -18,6 +18,7 @@ import { cn } from "@/lib/utils";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatItemProps {
   id: string;
@@ -57,7 +58,7 @@ export const ChatItem = ({
   socketQuery,
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
+  const { onOpen } = useModal();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -227,7 +228,12 @@ export const ChatItem = ({
           )}
           <ActionTooltip label="Delete">
             <Trash
-              onClick={() => setIsDeleting(true)}
+              onClick={() =>
+                onOpen("deleteMessage", {
+                  apiUrl: `${socketUrl}/${id}`,
+                  query: socketQuery,
+                })
+              }
               className="cursor-pointer bg-transparent ml-auto w-4 h-4 text-zinc-500 
             hover:text-zinc-600 dark:hover:text-zinc-300 transition"
             />
